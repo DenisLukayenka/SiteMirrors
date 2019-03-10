eb54e603a224a40df429a9f023bbf13e4b490320
Denis Lukayenka <denis.lukayenka@gmail.com>
2019-03-10 18:34:14 +0300

    Merge feature/HttpClient

--- Mirror/Mirror/Program.cs
+++ Mirror/Mirror/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using SiteLogic;
 
 namespace Mirror
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            SiteWorker worker = new SiteWorker("https://pornhub.com/", @"E:\Epam\epam-lab\Sites\", 1);
+
+            Task.WaitAll(worker.CreateLocalCopy());
+
+            Console.WriteLine("Completed!");
+            Console.ReadKey();
         }
     }
 }
